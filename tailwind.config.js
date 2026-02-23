74fd4087b219ae30fb3332dd33bfd8a8035ae149
monicasayon <98728864+monicasayon@users.noreply.github.com>
2026-02-23 23:36:39 +0800

    Remodel project ready for Netlify

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"], // Include files in src/ so Tailwind can scan them
+  theme: {
+    extend: {
+      keyframes: {
+        flip: {
+          '0%, 18%': { transform: 'translateY(0)' },
+          '20%, 38%': { transform: 'translateY(-20%)' },
+          '40%, 58%': { transform: 'translateY(-40%)' },
+          '60%, 78%': { transform: 'translateY(-60%)' },
+          '80%, 98%': { transform: 'translateY(-80%)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+      },
+
+      animation: {
+        flip: 'flip 10s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite',
+      },
+
+      backgroundImage: {
+        'eng1' : 'url("../src/bnw.webp")',
+        'card1' : 'url("../src/card1.webp")',
+        'card2' : 'url("../src/card2.webp")',
+        'card3' : 'url("../src/card3.webp")',
+        'card4' : 'url("../src/card4.webp")',
+        'card5' : 'url("../src/card5.webp")',
+        'bg2' : 'url("../src/bg2.webp")',
+        'bg1' : 'url("../src/bg1.webp")',
+      },
+
+      fontFamily: {
+        'nevis': ['Nevis', 'sans-serif'],
+        'manrope': ['Manrope', 'sans-serif'],
+        'menata': ['Menata', 'sans-serif'], 
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
