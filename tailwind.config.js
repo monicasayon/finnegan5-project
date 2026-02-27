943c8815d06fdd63bb5b804821651b6636ade678
monicasayon <98728864+monicasayon@users.noreply.github.com>
2026-02-27 19:04:35 +0800

    contact and about page
    Added contact and about page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"], // Include files in src/ so Tailwind can scan them
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        flip: 'flip 10s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite',
+        marquee: 'marquee 25s linear infinite',
+      },
+
       keyframes: {
         flip: {
           '0%, 18%': { transform: 'translateY(0)' },
@@ -12,27 +17,28 @@ module.exports = {
           '80%, 98%': { transform: 'translateY(-80%)' },
           '100%': { transform: 'translateY(-100%)' },
         },
-      },
-
-      animation: {
-        flip: 'flip 10s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite',
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
       },
 
       backgroundImage: {
-        'eng1' : 'url("../src/bnw.webp")',
-        'card1' : 'url("../src/card1.webp")',
-        'card2' : 'url("../src/card2.webp")',
-        'card3' : 'url("../src/card3.webp")',
-        'card4' : 'url("../src/card4.webp")',
-        'card5' : 'url("../src/card5.webp")',
-        'bg2' : 'url("../src/bg2.webp")',
-        'bg1' : 'url("../src/bg1.webp")',
+        'eng1': 'url("../src/bnw.webp")',
+        'card1': 'url("../src/card1.webp")',
+        'card2': 'url("../src/card2.webp")',
+        'card3': 'url("../src/card3.webp")',
+        'card4': 'url("../src/card4.webp")',
+        'card5': 'url("../src/card5.webp")',
+        'bg2': 'url("../src/bg2.webp")',
+        'bg1': 'url("../src/bg1.webp")',
+        'bg5': 'url("../src/bg5.webp")',
       },
 
       fontFamily: {
         'nevis': ['Nevis', 'sans-serif'],
         'manrope': ['Manrope', 'sans-serif'],
-        'menata': ['Menata', 'sans-serif'], 
+        'menata': ['Menata', 'sans-serif'],
         'montserrat': ['Montserrat', 'sans-serif'],
       },
     },
